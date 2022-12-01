01dca968d8cfa78d13abc7e66031e932bf07e0b4
Shailesh-Jaiswal <Shailesh-Jaiswal@users.noreply.github.com>
2022-12-01 12:02:26 +0000

    deploy: fcbtech/letstranzact-new-landing@9ce652018dc37d7acecf9e8193c88fec2caeb890

--- _nuxt/991fb1b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[51],{1147:function(n,t,e){"use strict";e.r(t);var c={asyncData:function(n){return(0,n.redirect)(301,"https://app.letstranzact.com/v2/login")}},l=e(33),component=Object(l.a)(c,undefined,undefined,!1,null,null,null);t.default=component.exports}}]);
