b80a9f5c8e6a33b290988f559c562cbbd9ac2b2d
zdhruvit <zdhruvit@users.noreply.github.com>
2023-02-24 08:50:17 +0000

    deploy: fcbtech/letstranzact-new-landing@d3e3d168faffeda21257e02a67c29542cb45f81d

--- _nuxt/static/1677054670/login/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1677054670",layout:"default",error:a,serverRendered:false,routePath:"\u002Flogin",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a28bc6a2"}}}}(null));
